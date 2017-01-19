c1198085fc673e4f57e428f81fa6dceb6e08ffe4
Sharhad Bashar <Sharhad@vpn65042.vpn.mcgill.ca>
2017-01-19 12:07:53 -0500

    cleaned up code

--- scripts/functions.py
+++ scripts/functions.py
@@ -12,6 +12,9 @@ import csv
 import datetime
 import numpy as np
 from numpy.linalg import inv
+import matplotlib.pyplot as plt 
+from numpy.linalg import linalg
+import seaborn as sns
 
 #Multiply matricies        
 def multiply(A,B):
@@ -20,7 +23,7 @@ def multiply(A,B):
 #Inverse matrix
 def inverse (A):
     m = 10^-6
-    Ainv = linalg.inv(A + np.eye(A.shape[1])*m)
+    Ainv = linalg.inv(A + np.eye(A.shape[1]) * m)
     return Ainv
 
 #Add matricies
@@ -46,8 +49,11 @@ def addOnes(X,size):
 #Calculate the weights
 def weights(X,Y):
 #w = (X^T*X)^-1X^T*Y
-    Xt = transpose(X)
+    X = addOnes(X,2)
     X = transpose(X)
+
+    Xt = transpose(X)
+
     temp1 = inverse(multiply(Xt,X))
     temp2 = multiply(Xt,Y)
     w = multiply(temp1,temp2)
@@ -64,7 +70,7 @@ def gradError(w,X,Y):
     return multiply(2,err)
 
 def getData():
-    data = np.loadtxt(open("miamimarathon/data/data.csv","rb"),delimiter=",",skiprows=1)
+    data = np.loadtxt(open("../data/data.csv","rb"),delimiter=",",skiprows=1)
     numData = len(data)
     ID = []
     age = []
@@ -74,8 +80,8 @@ def getData():
     pace = []
     year = []
     for i in range(numData):
-        #if (data[i][3] !=2016 and data[i][6] == 1):
-        if (data[i][6] == 1):
+        if (data[i][3] !=2016 and data[i][6] == 1):
+        #if (data[i][6] == 1):
             ID.append     (data[i][0])
             age.append    (data[i][1])
             rank.append   (data[i][2])
@@ -86,64 +92,7 @@ def getData():
         
     return ID,age,gender,rank,time,pace,year
     
-def ageVpace():
-    ID,age,gender,rank,time,pace,year = getData()    
-    plt.plot(age,pace)    
-    print ("Age vs. Pace")
-    w = weights(age,pace)
-    print w
-    plt.show()
 
-def yearVpace():
-    ID,age,gender,rank,time,pace,year = getData()    
-    plt.plot(year,pace)    
-    print ("Year vs. Pace")
-    w = weights(year,pace)
-    print w
-    plt.show()
-    
-def numOfMarVpace():
-    ID,age,gender,rank,time,pace,year = getData() 
-    rep = [[]for x in range(13)]
-    maxID = int(ID[len(ID)-1])
-    for i in range (1,maxID):
-        index = ID.count(float(i))
-        
-        if (index == 1):
-            rep[0].append(pace[ID.index(i)])
-        elif (index == 2):
-            rep[1].append(pace[ID.index(i)])
-        elif (index == 3):
-            rep[2].append(pace[ID.index(i)])
-        elif (index == 4):
-            rep[3].append(pace[ID.index(i)])
-        elif (index == 5):
-            rep[4].append(pace[ID.index(i)])
-        elif (index == 6):
-            rep[5].append(pace[ID.index(i)])
-        elif (index == 7):
-            rep[6].append(pace[ID.index(i)])
-        elif (index == 8):
-            rep[7].append(pace[ID.index(i)])
-        elif (index == 9):
-            rep[8].append(pace[ID.index(i)])
-        elif (index == 10):
-            rep[9].append(pace[ID.index(i)])
-        elif (index == 11):
-            rep[10].append(pace[ID.index(i)])
-        elif (index == 12):
-            rep[11].append(pace[ID.index(i)])
-        elif (index == 13):
-            rep[12].append(pace[ID.index(i)])
-
-    for i in range(13):
-        sns.stripplot(x=i, y="pace", data=rep[i]);   
-    print ("# of appareance  vs. Pace")
-    plt.show
-    
-#ageVpace()
-#yearVpace()    
-numOfMarVpace()        
 
 
        
